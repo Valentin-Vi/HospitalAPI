6a98f7c5eac08759a6fdaa635c4f9efdcb3a1919
0xxi1 <valentin.villa@protonmail.com>
2024-07-16 13:36:46 -0300

    Deleted unnecesary files. Implemented read and readALl methods for users table. Made naming convention changes. Changed node.js 'dev' command to accommodate naming changes.

--- src/database.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from 'pg';
-
-export const pool = new Pool({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "admin",
-    database: "hospital-api-v2"
-})
